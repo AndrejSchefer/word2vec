7c116554fe9dd82b56ece03657fa2f957424ce8f
Andrej Schefer <Andrej.Schefer@wbstraining.de>
2018-08-24 15:58:34 +0200

    englisch stop wörter

--- public_html/js/makeATensorflowModel.js
+++ public_html/js/makeATensorflowModel.js
@@ -60,11 +60,15 @@ $(document).ready(function () {
             ' ist ',
             ' a ',
             'will', ' be ', ' im', ' der', ' / ', ' mit ', ' aus ', ' dem ', ' und', ' auf ', ' unter '];
+        
+        
+
+        var stop_words = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"];
         var results = [];
         for (var i = 0; i < corpus.length; i++) {
             var res = corpus[i];
             for (var j = 0; j < stop_words.length; j++) {
-                res = res.replace(stop_words[j], ",");
+                res = res.replace(' '+stop_words[j]+' ', ",");
                 res = res.replace(",", " ");
             }
             dataW = [];
