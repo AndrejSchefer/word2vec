1af6022ad27fcb3542b4f378cdad0a9ff0e2d039
Andrej Schefer <root@v14618.1blu.de>
2018-08-25 11:26:50 +0200

    style

--- public_html/js/makeATensorflowModel.js
+++ public_html/js/makeATensorflowModel.js
@@ -39,30 +39,7 @@ $(document).ready(function () {
 
     function remove_stop_words(corpus) {
         $('.status').append('<div class="remove_stop_words state">remove stop words</div>');
-        var stop_words = [
-            ' wird ',
-            ' eine ',
-            ' ein ',
-            ' den ',
-            ' Der ',
-            ' der ',
-            ' die ',
-            ' von ',
-            ' vom ',
-            ' er ',
-            ' in ',
-            ' vor ',
-            ' vor ',
-            ' des ',
-            ' aber ',
-            ' in ',
-            ' den ',
-            ' ist ',
-            ' a ',
-            'will', ' be ', ' im', ' der', ' / ', ' mit ', ' aus ', ' dem ', ' und', ' auf ', ' unter '];
-        
-        
-
+    
         var stop_words = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"];
         var results = [];
         for (var i = 0; i < corpus.length; i++) {
@@ -116,11 +93,6 @@ $(document).ready(function () {
         }
 
         console.log(wordTable);
-
-        //$.each(wordTable, function (word, counter){
-        //    console.log(word+' '+counter);
-        //});
-
         return wordTable;
     }
 
@@ -194,6 +166,9 @@ $(document).ready(function () {
         return data;
     }
 
+    hiddenUnits = parseInt($('#hiddenUnits').val());
+    activation = $('#activation').val();
+
     function buildAModule() {
 
         var encodeNumberLength = 0;
@@ -209,22 +184,24 @@ $(document).ready(function () {
 
         encodeNumberLength = fnw[0]['wordEncode'].length;
         const hidden = tf.layers.dense({
-            units: 100, // Number of nodes in the hidden Layer
+            units: hiddenUnits, // Number of nodes in the hidden Layer
             inputShape: [encodeNumberLength], // Input Layer with 2 nodes
-            activation: "softmax"
+            activation: activation
         });
 
         model.add(hidden);
 
         const output = tf.layers.dense({
             units: encodeNumberLength,
-            activation: "softmax"
+            activation: activation            
         });
         model.add(output);
+        
         model.compile({
             loss: 'meanSquaredError',
             optimizer: 'sgd'
         });
+        
         $.each(fnw, function (key, value) {
             xs.push(fnw[key]['wordEncode']);
             ys.push(fnw[key]['neighborEncode']);
